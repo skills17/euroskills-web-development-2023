fd321396c0ad6450d7027cab550aaf9d50f6c870
Sebastian Häni <haeni.sebastian@gmail.com>
2023-06-26 00:25:02 +0200

    add module E, task 1: automated tests
    Signed-off-by: Sebastian Häni <haeni.sebastian@gmail.com>

---
+++ src/modules/module-e/handout-competitor/task1/src/QuestionLoader.test.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const { QuestionLoader } = require("./QuestionLoader");
+const { SingleAnswerStrategy } = require("./strategies/SingleAnswerStrategy");
+const {
+    MultipleChoiceStrategy,
+} = require("./strategies/MultipleChoiceStrategy");
+const {
+    MultipleCorrectAnswersStrategy,
+} = require("./strategies/MultipleCorrectAnswersStrategy");
+const { Question } = require("./Question");
+
+describe("QuestionLoader", () => {});
