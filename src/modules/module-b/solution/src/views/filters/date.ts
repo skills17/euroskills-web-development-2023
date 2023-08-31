52846ed6df77574fac7177fdd0aa8c2fe56443fa
Cyril Wanner <info@cyr.li>
2023-08-31 21:52:06 +0200

    Implement PR feedback

--- src/modules/module-b/solution/src/views/filters/date.ts
+++ src/modules/module-b/solution/src/views/filters/date.ts
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
 
-export const dateFilter = (value: Date, dateFormat: string = 'dd.MM.yyyy') => format(value, dateFormat);
+export const dateFilter = (value: Date, dateFormat: string = 'yyyy-MM-dd') => format(value, dateFormat);
 
-export const dateTimeFilter = (value: Date, dateTimeFormat: string = 'dd.MM.yyyy HH:mm') => dateFilter(value, dateTimeFormat);
+export const dateTimeFilter = (value: Date, dateTimeFormat: string = 'yyyy-MM-dd HH:mm') => dateFilter(value, dateTimeFormat);
