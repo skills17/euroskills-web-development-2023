18629176f4fa84860a4979c0d48670714a734407
Sebastian Häni <haeni.sebastian@gmail.com>
2023-09-05 15:42:21 +0200

    record service usage
    Signed-off-by: Sebastian Häni <haeni.sebastian@gmail.com>

--- src/modules/module-c/solution/src/controllers/api/imageRecognition.ts
+++ src/modules/module-c/solution/src/controllers/api/imageRecognition.ts
@@ -2,6 +2,9 @@ import {Request, Response} from "express";
 import FormData from "form-data";
 import * as fs from "fs";
 import fetch from "node-fetch";
+import {ServiceUsage} from "../../entities/ServiceUsage";
+import {Service} from "../../entities/Service";
+import {ApiToken} from "../../entities/ApiToken";
 
 async function recognize(req: Request, res: Response) {
 
@@ -9,6 +12,8 @@ async function recognize(req: Request, res: Response) {
         // Create a FormData object and append the file from the multipart form
         const formData = new FormData();
         formData.append('image', fs.createReadStream(req.file.path));
+
+        const start = new Date();
 
         // Forward the FormData to the external endpoint using node-fetch
         const response = await fetch('http://127.0.0.1:9003/recognize', {
@@ -19,9 +24,15 @@ async function recognize(req: Request, res: Response) {
             },
         });
 
+        const serviceUsage = new ServiceUsage();
+        serviceUsage.durationInMs = new Date().getTime() - start.getTime();
+        serviceUsage.service = await Service.findOneOrFail({where: {name: "MindReader"}});
+        serviceUsage.apiToken = await ApiToken.findOneOrFail({where: {token: req.header('X-API-TOKEN')}})
+        serviceUsage.usageStartedAt = start;
+        await serviceUsage.save();
+
         // Parse the response from the external endpoint
         const parsedData = await response.json();
-        console.log('parsedData', parsedData.objects)
 
         res.send({
             objects: parsedData.objects.map(obj => ({
